e771fb299e06564b47e331769daf3960fc4b5776
flapjackstan <elm.s.camargo@gmail.com>
2020-05-29 17:45:36 -0700

    updated scraper

--- image_scrape.py
+++ image_scrape.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from pandas.io.json import json_normalize
 import google_streetview.api
 import os
 
-
+os.chdir('C:/Users/elmsc/Documents/gis/image_scrape')
 
 #https://maps.googleapis.com/maps/api/streetview?size=1200x800&pano=iOsxtpWAa-yq5of4v9BJRg&key=AIzaSyCfCXdXfjYPd8nAii9dPFVPUjYIBzBf4vo
 
@@ -10,50 +11,43 @@ images = pd.read_csv('data/2017-panorama.csv')
 
 images.head()
 
+images['panoramaID'].nunique()
+
+#test = pd.DataFrame()
+
+#test['unique'] = images['panoramaID'] + '_' + images['UID'] + '_' + images['parkName']
+#test['unique'].nunique()
+#dups = test[test.duplicated(['unique'], keep=False)]
+# exactly 210 duplicates
+
 #TODO ADD METADATA TO CSV
+meta_col = ['date', 'pano_id', 'status', 'lat', 'lng']
+meta = pd.DataFrame(columns = meta_col)
+del(meta_col)
 
 
-#for i in images['panoramaID'].head(5):
-#    params = [{
-#            'size': '640x640',
-#            'pano': i,
-#            'key': 'AIzaSyCfCXdXfjYPd8nAii9dPFVPUjYIBzBf4vo'}]
-#
-#    api_list = google_streetview.helpers.api_list(params) 
-#    
-#    results = google_streetview.api.results(api_list)
-#    
-#    
-#    results.download_links('downloads')
-#    os.rename('downloads/gsv_0.jpg', 'downloads/hello.jpg')
-#    results.save_metadata('metadata.json')
+for i,j,k in zip(images['panoramaID'],images['UID'],images['parkName']):
+    params = [{
+            'size': '640x640',
+            'pano': i,
+            'key': 'AIzaSyCfCXdXfjYPd8nAii9dPFVPUjYIBzBf4vo'}]
+
+    results = google_streetview.api.results(params)
+    results.download_links('downloads')
     
+    #NEED TO NAME JPG BY UNIQUE ID NOT PANO ID
     
+    os.rename('downloads/gsv_0.jpg', 'downloads/' + i + '_' + j + '_' + k .+ 'jpg')
+    results.save_metadata('downloads/metadata.json')
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-#pans = images['panoramaID']
-#pans.iloc[:5]
-#
-#params = [{
-#        'size': '640x640',
-#        'pano': pans.iloc[:5],
-#        'key': 'AIzaSyCfCXdXfjYPd8nAii9dPFVPUjYIBzBf4vo'}]
-#
-#api_list = google_streetview.helpers.api_list(params)
-#
-#results = google_streetview.api.results(api_list)
-#results.download_links('downloads')
-#results.save_metadata('metadata2.json')
+    temp_meta = pd.read_json('downloads/metadata.json')
+    location = pd.DataFrame(json_normalize(temp_meta['location']))
+
+    temp_meta = temp_meta.drop(columns = ['location', 'copyright', '_file'])
+    temp_meta = pd.concat([temp_meta,location],axis=1)
+    meta = meta.append(temp_meta, ignore_index = True)
+    del(location,temp_meta)
+
+
+meta = meta.rename(columns = {'pano_id': 'panoramaID'})
+images = images.merge(meta, on = 'panoramaID', how = 'left')
